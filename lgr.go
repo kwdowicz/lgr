c3cb3c7259eba85d3875fd1079b9ab6157d2fc75
Kamil Wdowicz <kwdowicz@gmail.com>
2024-11-08 15:15:47 +0000

    logging 2

--- lgr.go
+++ lgr.go
@@ -9,13 +9,13 @@ import (
 func LogMethods(target interface{}) {
 	v := reflect.ValueOf(target).Elem()
 	t := v.Type()
-	log.Println("(log)In LogMethods")
+	log.Printf("In LogMethods: %v\n", v)
 
 	for i := 0; i < v.NumMethod(); i++ {
 		method := v.Method(i)
 		methodType := t.Method(i)
-		log.Printf("method: %v", method)
-		log.Printf("method type: %v", method)
+		log.Printf("method: %v\n", method)
+		log.Printf("method type: %v\n", method)
 		wrappedMethod := func(in []reflect.Value) []reflect.Value {
 			log.Printf("Entering method %s", methodType.Name)
 			fmt.Printf("(fmt)Entering method %s", methodType.Name)
