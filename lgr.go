7b0ae4c8f1d0c27200dc728d0f58bab7014de6fe
Kamil Wdowicz <kwdowicz@gmail.com>
2024-11-08 15:08:58 +0000

    latest cleanup

--- lgr.go
+++ lgr.go
@@ -1,19 +1,26 @@
 package lgr
 
 import (
-	"reflect"
+	"fmt"
 	"log"
+	"reflect"
 )
+
+func Check() {
+	fmt.Println("Checking...")
+}
 
 func LogMethods(target interface{}) {
 	v := reflect.ValueOf(target).Elem()
 	t := v.Type()
+	fmt.Println("In LogMethods")
 
 	for i := 0; i < v.NumMethod(); i++ {
 		method := v.Method(i)
 		methodType := t.Method(i)
 		wrappedMethod := func(in []reflect.Value) []reflect.Value {
 			log.Printf("Entering method %s", methodType.Name)
+			fmt.Printf("(fmt)Entering method %s", methodType.Name)
 			result := method.Call(in)
 			return result
 		}
